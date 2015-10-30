878ffe65af5677bd18638648fad3f5cb9bbf16e1
Sergio Jimenez Corominas <sergio@claimsoluciones.com>
2015-10-30 18:22:27 +0100

    first commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+add_import_path "bower_components/Hover/scss/"
+add_import_path "bower_components/bootstrap-sass-official/assets/stylesheets/"
+add_import_path "bower_components/font-awesome/scss/"
+
+relative_assets = true
+
